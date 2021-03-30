389270230d20f204774a1f076a830b6629deff4d
Yasuhiro Ishii <73149353+yasu971@users.noreply.github.com>
2021-03-30 11:09:16 +0900

    arrary.rb

---
+++ array.rb
@@ -0,0 +1,15 @@
+pencil_case = ["ペン", "消しゴム", "定規"]
+puts pencil_case.length
+puts pencil_case
+
+pencil_case << "えんぴつ"
+pencil_case << "付箋"
+puts pencil_case.length
+puts pencil_case
+
+animals = ["イヌ", "キリン", "ライオン"]
+puts animals
+animals << "パンダ"
+puts animals
+animals[0] = "ネコ"
+puts animals
